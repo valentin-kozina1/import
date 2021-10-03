34608c38acd452443ece975e23b4e88b40380f99
valentin-kozina1 <valentin.kozina1@gmail.com>
2021-10-03 17:06:53 +0300

    edit files

--- js/wave.js
+++ js/wave.js
@@ -1,128 +1,14 @@
-/*class WaveGlitch {
-    constructor(
-        id,
-        src,
-        width,
-        height,
-        posX = 0,
-        posY = 0,
-        waveAmplitude = 10,
-        intensity = 2,
-        speed = 0.10,
-    ) {
-        this.src  = src
-        this.posX = posX
-        this.posY = posY
-        this.requestAnim = null
-
-        // get image
-        this.image     = new Image()
-        this.image.src = this.src
-        this.imgWidth  = width
-        this.imgHeight = height
-
-        // get canvas
-        this.canvas       = document.querySelector(`${ id }`)
-        this.ctx          = this.canvas.getContext('2d')  // получаем контекст canvas
-        this.canvasWidth  = this.imgWidth
-        this.canvasHeight = this.imgHeight
-        this.canvas.setAttribute('width', this.canvasWidth)   // width  === ширине img
-        this.canvas.setAttribute('height', this.canvasHeight) // height === ширине img
-
-        // cut image
-        this.imgSlice      = this.imgWidth / 2  // кол-во кусочков на которое обрезается img
-        this.imgSliceWidth = 2                  // ширина кусочка
-
-        // control animation
-        this.waveAmplitude = waveAmplitude
-        this.intensity     = intensity
-        this.speed         = 0
-        this.speedPlus     = speed
-    }
-
-    startAnimate = () => {
-        this.speed += this.speedPlus
-        this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
-
-        for (let i = 0; i <= this.imgSlice; i++) {
-            this.ctx.drawImage(
-                this.image,
-
-                i * this.imgSliceWidth - this.posX,
-                Math.sin(this.speed - (i / this.waveAmplitude)) * this.intensity,
-                this.imgSliceWidth,
-                this.imgHeight,
-
-                i * this.imgSliceWidth,
-                this.posY,
-                this.imgSliceWidth,
-                this.imgHeight,
-            )
-        }
-
-        this.requestAnim = requestAnimationFrame(this.startAnimate)
-    }
-
-    init = () => {
-        this.image.addEventListener('load', this.startAnimate)
-    }
-
-    destroy = () => {
-        // stop animation
-        cancelAnimationFrame(this.requestAnim)
-    }
-}
-
- /*const draw = new Draw(
- 		this.ctx.drawImage(image, 0, 0)
- 	)*/
-    	
-
-/*const waveGlitch = new WaveGlitch(
-    '#canvas',         // id элемента, или селектор
-    'img/negotiation-foto0.jpg',   // путь до картинки(от HTML)
-    300,
-    245,
-
-    // ↓ необязательные параметры ↓
-    0,
-    0,
-    10,
-    10,
-    0.01, // значение от 0.1 до 1 (0.01, 0.001 и тд)
-)
-
-waveGlitch.init()
-
-this.canvas.onmouseover = function()
-		{ 
-			waveGlitch.init() // вызвать анимацию
-		}
-		
-
-	this.canvas.onmouseout = function()
-		{       	
-			waveGlitch.destroy();   // остановить анимацию
-		}*/
-
-
-// waveGlitch.destroy() // остановить анимацию
-// для канваса width и height установятся автоматически !
-
-
-//___________________________________________________
-
-
 let canvas = document.getElementsByClassName('canvas');
+let canvas_foto = document.getElementsByClassName('negotiation-foto-first');
+let left = document.getElementById('left');
+let right = document.getElementById('right');
+
 let ctx = Array();
 let canvasWidth = canvas[0].width;
 let canvasHeight = canvas[0].height;
 let image = Array();
 let requestId;
 let k;
-let left = document.getElementById('left');
-let right = document.getElementById('right');
-let canvas_foto = document.getElementsByClassName('negotiation-foto-first');
 let speed = 0;
 let imgWidth = 300/2;
 let imgHeight = 245;
